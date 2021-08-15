bdce58f8bdb3e538e516734ad735f1ef2349b5ec
Nikhil George <nikhilgeo@gmail.com>
2021-08-15 20:43:59 +0530

    Remove unused imports

--- app/src/main/java/com/vedantamadam/raksha/MyGlobalClass.java
+++ app/src/main/java/com/vedantamadam/raksha/MyGlobalClass.java
@@ -1,14 +1,9 @@
 package com.vedantamadam.raksha;
 
-import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.SharedPreferences;
-import android.provider.ContactsContract;
 import android.telephony.SmsManager;
-import android.view.View;
 import android.widget.Toast;
-
 
 import com.gun0912.tedpermission.PermissionListener;
 import com.gun0912.tedpermission.TedPermission;
@@ -17,7 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MyGlobalClass {
-    public static String phoneNumber1 = "", phoneNumber2 = "", senstivityNumber = "",timestamp;
+    public static String phoneNumber1 = "", phoneNumber2 = "", senstivityNumber = "", timestamp;
     public static long time_startLocUpdates, time_onLocationResult;
     public static boolean fall = true;
     public static boolean permission_approved = false;
@@ -27,7 +22,7 @@ public class MyGlobalClass {
         SharedPreferences pref = context.getSharedPreferences("com.vedantamadam.raksha.PREFERENCE", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
         if (value != null && !value.isEmpty()) //write only if value is not null and not empty
-        editor.putString(key, value);
+            editor.putString(key, value);
         editor.apply();
     }
 
@@ -35,7 +30,7 @@ public class MyGlobalClass {
         String value;
         SharedPreferences pref = context.getSharedPreferences("com.vedantamadam.raksha.PREFERENCE", Context.MODE_PRIVATE);
         if (pref.contains(key)) {
-            value = pref.getString(key,"");
+            value = pref.getString(key, "");
         } else {
             value = null;
         }
@@ -59,40 +54,19 @@ public class MyGlobalClass {
 
     }
 
-    public static String time_forLocFetch(){
-        try{
-            long time_duration = time_onLocationResult - time_startLocUpdates ;
+    public static String time_forLocFetch() {
+        try {
+            long time_duration = time_onLocationResult - time_startLocUpdates;
             time_startLocUpdates = 0;
             time_onLocationResult = 0;
             return String.valueOf(time_duration);
-        } catch (Exception ex){
+        } catch (Exception ex) {
 
             return null;
         }
     }
 
-    public void sendSMS(Context context, String msg) {
-        try {
-            SmsManager sms = SmsManager.getDefault();
-            ArrayList<String> emerMsg;
-            emerMsg = sms.divideMessage(msg);
-            if (if_sosnumber_exist(context)) {
-                String[] ph = {read_pref(context, "e1"), read_pref(context, "e2")};
-                for (int i = 0; i < ph.length && ph[i] != null; i++) {
-                    sms.sendMultipartTextMessage(ph[i], null, emerMsg, null, null);
-                    Toast.makeText(context, "Successfully send SOS to "+ph[i].toString(), Toast.LENGTH_SHORT).show();
-                }
-            } else {
-                Toast.makeText(context, "Please set SOS contact numbers.", Toast.LENGTH_SHORT).show();
-            }
-        }catch (Exception ex){
-            Toast.makeText(context, "Failed to send SOS", Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
-    public static boolean checkPermission(String mPermission, Context context, String deniedMessage)
-    {
+    public static boolean checkPermission(String mPermission, Context context, String deniedMessage) {
         new TedPermission()
                 .with(context)
                 .setPermissionListener(new PermissionListener() {
@@ -106,13 +80,32 @@ public class MyGlobalClass {
                         permission_approved = false;
                     }
                 })
-                .setDeniedMessage( deniedMessage)
+                .setDeniedMessage(deniedMessage)
                 .setDeniedCloseButtonText(android.R.string.ok)
                 .setGotoSettingButton(true)
                 .setPermissions(mPermission)
                 .check();
 
         return permission_approved;
+    }
+
+    public void sendSMS(Context context, String msg) {
+        try {
+            SmsManager sms = SmsManager.getDefault();
+            ArrayList<String> emerMsg;
+            emerMsg = sms.divideMessage(msg);
+            if (if_sosnumber_exist(context)) {
+                String[] ph = {read_pref(context, "e1"), read_pref(context, "e2")};
+                for (int i = 0; i < ph.length && ph[i] != null; i++) {
+                    sms.sendMultipartTextMessage(ph[i], null, emerMsg, null, null);
+                    Toast.makeText(context, "Successfully send SOS to " + ph[i].toString(), Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(context, "Please set SOS contact numbers.", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception ex) {
+            Toast.makeText(context, "Failed to send SOS", Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
--- app/src/main/java/com/vedantamadam/raksha/SosActivity.java
+++ app/src/main/java/com/vedantamadam/raksha/SosActivity.java
@@ -10,17 +10,13 @@ import androidx.core.content.ContextCompat;
 
 
 import android.Manifest;
-import android.app.ActivityManager;
 import android.app.AlertDialog;
-import android.app.Application;
 import android.content.DialogInterface;
 import android.content.Intent;
 
 
-import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 
 
@@ -31,12 +27,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import com.gun0912.tedpermission.PermissionListener;
-import com.gun0912.tedpermission.TedPermission;
-
 import java.util.ArrayList;
-import java.util.List;
 
 
 public class SosActivity extends AppCompatActivity {
